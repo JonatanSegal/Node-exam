8638c008edc83abc07cec4a2e09c42d41c841c95
JonatanSegal <82033910+JonatanSegal@users.noreply.github.com>
2022-12-15 22:03:06 +0100

    First set up for the project and copied over sign up and login from mandatory 2

---
+++ Server/app.js
@@ -0,0 +1,49 @@
+import dotenv from "dotenv"
+dotenv.config()
+
+import express from "express"
+const app = express()
+
+import rateLimit from 'express-rate-limit'
+import session from "express-session"
+import helmet from "helmet"
+import cors from "cors"
+
+app.use(cors({ credentials: true, origin: true }))
+app.use(express.json())
+app.use(helmet())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }))
+
+const generalLimiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 80,
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(generalLimiter)
+
+const loginLimit = rateLimit({
+	windowMs: 5 * 60 * 1000, // 5 minutes
+	max: 5, // 5 requests per 5 minutes
+})
+app.use("/api/login", loginLimit)
+
+
+import authRouter from "./routers/authRouter.js"
+app.use(authRouter)
+
+
+app.get("*", (req, res) => {
+    res.send(`<h1>404</h1><br><h3>Could not find page</h3>`)
+})
+
+const PORT = 8080 || process.env.PORT
+app.listen(PORT, () => {
+    console.log(`APP is running on: ${PORT}`)
+})
+
